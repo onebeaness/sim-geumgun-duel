d93d5ec2e3add6ca79e4db4825737f7b320dd669
onebeaness <onebeaness@gmail.com>
2026-06-25 15:57:47 +0900

    Update app.py
    실력검정 제거

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import streamlit as st
 st.set_page_config(page_title="심: 금군 듀얼", page_icon="⚔️", layout="wide")
 
 lobby = st.Page("views/lobby.py", title="결투장", icon="⚔️", default=True)
-tutorial = st.Page("views/tutorial.py", title="실력 검정", icon="🎯")
 dashboard = st.Page("views/dashboard.py", title="대시보드", icon="📊")
 mydashboard = st.Page("views/selfdashboard.py", title="수제대시보드")
-st.navigation([lobby, tutorial, dashboard, mydashboard]).run()
+st.navigation([lobby, dashboard, mydashboard]).run()
